44fce8c9b02d4c0cb6eb94f5799ce2eb107e3dc8
kalzoo <22137047+kalzoo@users.noreply.github.com>
2022-03-07 09:34:30 -0500

    Chore: fmt

--- src/program/error.rs
+++ src/program/error.rs
@@ -5,10 +5,13 @@ use crate::instruction::Instruction;
 #[derive(Debug, Error)]
 pub enum ProgramError {
     #[error("invalid calibration `{0}`: {message}")]
-    InvalidCalibration { instruction: Instruction, message: String },
+    InvalidCalibration {
+        instruction: Instruction,
+        message: String,
+    },
 
     #[error("instruction {0} expands into itself")]
-    RecursiveCalibration(Instruction)
+    RecursiveCalibration(Instruction),
 }
 
 pub type ProgramResult<T> = Result<T, ProgramError>;
