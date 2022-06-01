e10749c69ae17d6541001a9ac4682e16d5b58e5b
Kalan <22137047+kalzoo@users.noreply.github.com>
2022-06-01 12:46:08 -0700

    Breaking: Improve Timing Graph (#61)
    * Move graphviz-dot format writer to own module
    
    * Install dot_writer
    
    * Breaking: revise graphviz dot generation API
    
    * Update: represent instructions as rectangles in program graphviz dot format
    
    * Fix: error in memory access dependencies
    
    * Merge branch 'main' into feat/improve-dot-graphs
    
    * Chore: add top-level crate docs with links
    
    * Prefix instruction text with [index] in dotfile output
    
    * Move graphviz dot utility behind a feature flag
    
    * Chore: Test with --all-features in CI
    
    * Fix: move graphviz-dot tests out to conditional module
    
    * Chore: update test snapshots to include instruction index
    
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>
    Co-authored-by: Steve Manuel <nilslice@gmail.com>
    Co-authored-by: Mark Skilbeck <mark.skilbeck@rigetti.com>

--- src/expression.rs
+++ src/expression.rs
@@ -1,18 +1,17 @@
-/**
- * Copyright 2021 Rigetti Computing
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
+// Copyright 2021 Rigetti Computing
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use num_complex::Complex64;
 use std::collections::{hash_map::DefaultHasher, HashMap};
 use std::f64::consts::PI;
@@ -799,7 +798,7 @@ mod tests {
         fn complexes_are_parseable_as_expressions(value in arb_complex64()) {
             let parsed = Expression::from_str(&format_complex(&value));
             assert!(parsed.is_ok());
-            assert_eq!(Expression::Number(value), parsed.unwrap().simplify());
+            assert_eq!(Expression::Number(value), parsed.unwrap().into_simplified());
         }
 
     }
--- src/instruction.rs
+++ src/instruction.rs
@@ -1,18 +1,17 @@
-/**
- * Copyright 2021 Rigetti Computing
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
+// Copyright 2021 Rigetti Computing
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, fmt};
 
@@ -886,7 +885,7 @@ impl Instruction {
     /// let mut instructions = program.to_instructions(true);
     /// instructions.iter_mut().for_each(|inst| inst.apply_to_expressions(Expression::simplify));
     ///
-    /// assert_eq!(instructions[0].to_string(), String::from("SHIFT-PHASE 0 \"rf\" 4"))
+    /// assert_eq!(instructions[0].to_string(), String::from("SHIFT-PHASE 0 \"rf\" 4.0"))
     ///
     /// ```
     pub fn apply_to_expressions(&mut self, mut closure: impl FnMut(&mut Expression)) {
