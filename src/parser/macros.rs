5450035e450ae2345b62054fa002cacd1b4b3997
Michael Bryant <mbryant@rigetti.com>
2022-10-25 07:42:14 -0700

    chore: remove extra TODO
    Co-authored-by: Mark Skilbeck <mark.skilbeck@rigetti.com>

--- src/parser/macros.rs
+++ src/parser/macros.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO: Internal variants?
 
 #[macro_export]
 macro_rules! expected_token {
