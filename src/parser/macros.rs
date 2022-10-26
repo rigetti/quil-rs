f8bfc20735228f21024b667891cbef7a72f1e95e
Michael Bryant <mbryant@rigetti.com>
2022-10-26 09:11:32 -0700

    style: run cargo fmt

--- src/parser/macros.rs
+++ src/parser/macros.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #[macro_export]
 macro_rules! expected_token {
     ($input: expr, $actual:expr, $expected:expr) => {{
