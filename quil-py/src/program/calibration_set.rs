45ed4c892aba97bd088a4f01e0cb8fdbdd7ab15f
marquessv <marquessavaldez@gmail.com>
2023-03-07 13:45:32 -0800

    implement a few PyCalibrationSet methods

--- quil-py/src/program/calibration_set.rs
+++ quil-py/src/program/calibration_set.rs
@@ -1,8 +1,14 @@
 use pyo3::prelude::*;
-use quil_rs::program::CalibrationSet;
-use rigetti_pyo3::{impl_repr, py_wrap_type, PyTryFrom};
+use quil_rs::{
+    expression::Expression,
+    instruction::{GateModifier, Qubit},
+    program::CalibrationSet,
+};
+use rigetti_pyo3::{impl_repr, py_wrap_type, PyTryFrom, PyWrapper};
 
-use crate::instruction::{PyCalibration, PyMeasureCalibrationDefinition};
+use crate::instruction::{
+    PyCalibration, PyExpression, PyGateModifier, PyMeasureCalibrationDefinition, PyQubit,
+};
 
 py_wrap_type! {
     PyCalibrationSet(CalibrationSet) as "CalibrationSet"
@@ -20,6 +26,33 @@ impl PyCalibrationSet {
             calibrations: Vec::<_>::py_try_from(py, &calibrations)?,
             measure_calibrations: Vec::<_>::py_try_from(py, &measure_calibrations)?,
         }))
+    }
+
+    pub fn get_match_for_gate(
+        &self,
+        py: Python<'_>,
+        gate_modifiers: Vec<PyGateModifier>,
+        gate_name: &str,
+        gate_parameters: Vec<PyExpression>,
+        gate_qubits: Vec<PyQubit>,
+    ) -> PyResult<Option<PyCalibration>> {
+        Ok(self
+            .as_inner()
+            .get_match_for_gate(
+                &Vec::<GateModifier>::py_try_from(py, &gate_modifiers)?,
+                gate_name,
+                &Vec::<Expression>::py_try_from(py, &gate_parameters)?,
+                &Vec::<Qubit>::py_try_from(py, &gate_qubits)?,
+            )
+            .map(PyCalibration::from))
+    }
+
+    pub fn __len__(&self) -> usize {
+        self.as_inner().len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.as_inner().is_empty()
     }
 }
 
