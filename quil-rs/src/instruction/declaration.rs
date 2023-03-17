7e58395346cfb4ef351749fda1c212e5310c7757
marquessv <marquessavaldez@gmail.com>
2023-03-17 14:39:28 -0700

    create Offset type, add it to Declaration, update Display

--- quil-rs/src/instruction/declaration.rs
+++ quil-rs/src/instruction/declaration.rs
@@ -53,18 +53,32 @@ impl fmt::Display for Vector {
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Offset {
+    offset: u64,
+    data_type: ScalarType,
+}
+
+impl fmt::Display for Offset {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {}", self.offset, self.data_type)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Declaration {
     pub name: String,
     pub size: Vector,
     pub sharing: Option<String>,
+    pub offsets: Vec<Offset>,
 }
 
 impl Declaration {
-    pub fn new(name: String, size: Vector, sharing: Option<String>) -> Self {
+    pub fn new(name: String, size: Vector, sharing: Option<String>, offsets: Vec<Offset>) -> Self {
         Self {
             name,
             size,
             sharing,
+            offsets,
         }
     }
 }
@@ -73,11 +87,30 @@ impl fmt::Display for Declaration {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "DECLARE {} {}", self.name, self.size)?;
         if let Some(shared) = &self.sharing {
-            write!(f, "SHARING {shared}")?
+            write!(f, "SHARING {shared}")?;
+            if !self.offsets.is_empty() {
+                write!(f, "OFFSET")?;
+                for offset in self.offsets {
+                    write!(f, " {offset}")?
+                }
+            }
         }
         Ok(())
     }
 }
+
+// TODO: Tests for fmt::Display
+// #[cfg(test)]
+// mod test_declaration {
+//     use super::{Declaration, Vector, Offset, ScalarType};
+//     use rstest::rstest;
+//
+//     #[rstest]
+//     #[case(
+//         "ro",
+//         Vector{ data_type: ScalarType::Bit, length: 1 }
+//     )]
+// }
 
 #[derive(Clone, Debug, Hash, PartialEq)]
 #[cfg_attr(test, derive(Arbitrary))]
