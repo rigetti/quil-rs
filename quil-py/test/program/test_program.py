54d518943b51f194ce3fe8db5d33d5003ee7958a
marquessv <marquessavaldez@gmail.com>
2023-03-21 09:09:49 -0700

    expand test program

--- quil-py/test/program/test_program.py
+++ quil-py/test/program/test_program.py
@@ -4,7 +4,15 @@ from quil.program import Program
 
 
 def test_pickle():
-    program = Program.parse("DECLARE ro BIT[6]")
+    program = Program.parse(
+        """
+DECLARE ro BIT[2]
+H 0
+CNOT 0 1
+MEASURE 0 ro[0]
+MEASURE 1 ro[1]
+"""
+    )
     pickled = pickle.dumps(program)
     unpickled = pickle.loads(pickled)
     assert program == unpickled
