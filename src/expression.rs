27c8a2f4e24af3241eab035204e7ef3cb2c99c25
Graham Enos <genos@rigetti.com>
2023-05-24 22:55:13 -0400

    chore: appease clippy in tests

--- src/expression.rs
+++ src/expression.rs
@@ -1323,8 +1323,8 @@ mod tests {
         fn exponentiation_works_as_expected(left in arb_expr(), right in arb_expr()) {
             let expected = Expression::Infix { left: Box::new(left.clone()), operator: InfixOperator::Caret, right: Box::new(right.clone()) };
             prop_assert_eq!(left.clone() ^ right.clone(), expected.clone());
-            let mut x = left.clone();
-            x ^= right.clone();
+            let mut x = left;
+            x ^= right;
             prop_assert_eq!(x, expected);
         }
 
@@ -1332,8 +1332,8 @@ mod tests {
         fn addition_works_as_expected(left in arb_expr(), right in arb_expr()) {
             let expected = Expression::Infix { left: Box::new(left.clone()), operator: InfixOperator::Plus, right: Box::new(right.clone()) };
             prop_assert_eq!(left.clone() + right.clone(), expected.clone());
-            let mut x = left.clone();
-            x += right.clone();
+            let mut x = left;
+            x += right;
             prop_assert_eq!(x, expected);
         }
 
@@ -1341,8 +1341,8 @@ mod tests {
         fn subtraction_works_as_expected(left in arb_expr(), right in arb_expr()) {
             let expected = Expression::Infix { left: Box::new(left.clone()), operator: InfixOperator::Minus, right: Box::new(right.clone()) };
             prop_assert_eq!(left.clone() - right.clone(), expected.clone());
-            let mut x = left.clone();
-            x -= right.clone();
+            let mut x = left;
+            x -= right;
             prop_assert_eq!(x, expected);
         }
 
@@ -1350,8 +1350,8 @@ mod tests {
         fn multiplication_works_as_expected(left in arb_expr(), right in arb_expr()) {
             let expected = Expression::Infix { left: Box::new(left.clone()), operator: InfixOperator::Star, right: Box::new(right.clone()) };
             prop_assert_eq!(left.clone() * right.clone(), expected.clone());
-            let mut x = left.clone();
-            x *= right.clone();
+            let mut x = left;
+            x *= right;
             prop_assert_eq!(x, expected);
         }
 
@@ -1359,8 +1359,8 @@ mod tests {
         fn division_works_as_expected(left in arb_expr(), right in arb_expr()) {
             let expected = Expression::Infix { left: Box::new(left.clone()), operator: InfixOperator::Slash, right: Box::new(right.clone()) };
             prop_assert_eq!(left.clone() / right.clone(), expected.clone());
-            let mut x = left.clone();
-            x /= right.clone();
+            let mut x = left;
+            x /= right;
             prop_assert_eq!(x, expected);
         }
 
