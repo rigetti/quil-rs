d93f65434d4a2b0fc9fc9d10baec61ea50737794
marquessv <marquessavaldez@gmail.com>
2023-06-01 14:28:18 -0700

    ci: try without cargo-edit

--- scripts/sync_versions.sh
+++ scripts/sync_versions.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 # This script gets the current version of quil-rs from its manifest, and uses it to update the dependency specification
 # of quil-rs in quil-py's manifset.
-# Requires that `jq` and `cargo-edit` are installed.
 
-COMMON_VERSION=$(cargo metadata --format-version 1 --no-deps | jq '.packages[] | select(.name=="quil-rs") | .version' -r)
-# Specifying the @VERSION here is required for cargo upgrade to ignore the `path` field for the package.
-cargo upgrade --package quil-rs@$COMMON_VERSION -i allow --recursive false --offline
+# Get current version of quil-rs
+PACKAGE_VERSION=$(cargo metadata --format-version 1 --no-deps | grep -o '"name":"quil-rs","version":"[^"]*' | cut -d '"' -f 8)
+
+# Write it to `quil-py`s Cargo.toml
+# `awk -i inplace` would work on linux, but not on macos. This command is compatible with both.
+awk -v ver="$PACKAGE_VERSION" '/^quil-rs = /{$0="quil-rs = { path = \"../quil-rs\", version = \"" ver "\" }"}1' quil-py/Cargo.toml > quil-py/Cargo.toml.tmp
+mv quil-py/Cargo.toml.tmp quil-py/Cargo.toml
+
+# Validate that the new version of Cargo.toml is valid
+cargo check --manifest-path=quil-py/Cargo.toml
