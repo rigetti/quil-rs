ad0b32b3e4b03db88e15541c643b1bf1b79e476c
kalzoo <22137047+kalzoo@users.noreply.github.com>
2023-06-22 13:49:54 -0700

    fix benchmark

--- quil-rs/benches/get_frames_for_instruction.rs
+++ quil-rs/benches/get_frames_for_instruction.rs
@@ -6,33 +6,23 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn benchmark_quil_corpus(c: &mut Criterion) {
     corpus::from_corpus().iter().for_each(|cfg| {
-        for include_blocked in [true, false] {
-            c.bench_function(
-                &format!(
-                    "{} ({}include blocked)",
-                    cfg.name,
-                    if include_blocked { "" } else { "no " }
-                ),
-                |b| {
-                    b.iter_batched(
-                        || {
-                            quil_rs::Program::from_str(&cfg.program)
-                                .expect("program should parse successfully")
-                        },
-                        |prog| {
-                            for instruction in &prog.instructions {
-                                for _ in 0..50 {
-                                    let frames = prog
-                                        .get_frames_for_instruction(instruction, include_blocked);
-                                    black_box(frames);
-                                }
-                            }
-                        },
-                        criterion::BatchSize::SmallInput,
-                    )
+        c.bench_function(&cfg.name, |b| {
+            b.iter_batched(
+                || {
+                    quil_rs::Program::from_str(&cfg.program)
+                        .expect("program should parse successfully")
                 },
-            );
-        }
+                |prog| {
+                    for instruction in &prog.instructions {
+                        for _ in 0..50 {
+                            let frames = prog.get_frames_for_instruction(instruction);
+                            black_box(frames);
+                        }
+                    }
+                },
+                criterion::BatchSize::SmallInput,
+            )
+        });
     })
 }
 
