cf76bcec691d8f8dfa6a632293f64d9c5ae4164a
Marquess Valdez <marquessavaldez@gmail.com>
2023-08-22 15:47:37 -0700

    print condition in conditional jump instructions

--- quil-rs/src/instruction/control_flow.rs
+++ quil-rs/src/instruction/control_flow.rs
@@ -151,7 +151,7 @@ impl Quil for JumpWhen {
     ) -> Result<(), crate::quil::ToQuilError> {
         write!(writer, "JUMP-WHEN ")?;
         self.target.write(writer, fall_back_to_debug)?;
-        write!(writer, " ")?;
+        write!(writer, " {}", self.condition)?;
         Ok(())
     }
 }
@@ -177,6 +177,7 @@ impl Quil for JumpUnless {
         write!(writer, "JUMP-UNLESS ")?;
         self.target.write(writer, fall_back_to_debug)?;
         write!(writer, " ")?;
+        write!(writer, " {}", self.condition)?;
         Ok(())
     }
 }
