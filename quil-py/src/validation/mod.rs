d7d16d57d4440d7bb5bc5032ceddccc3e5ad4ce3
Marquess Valdez <marquessavaldez@gmail.com>
2023-11-22 10:18:45 -0800

    revert test changes to validation module

--- quil-py/src/validation/mod.rs
+++ quil-py/src/validation/mod.rs
@@ -1,15 +1,7 @@
-use pyo3::{pyfunction, PyResult};
-
 use rigetti_pyo3::create_init_submodule;
 
 pub mod identifier;
 
 create_init_submodule! {
-    funcs: [dummy],
-    submodules: ["identifier": identifier::init_submodule],
-}
-
-#[pyfunction]
-fn dummy() -> PyResult<()> {
-    Ok(())
+    submodules : ["identifier": identifier::init_submodule],
 }
