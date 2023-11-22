3aacac7b25c79feeefdc4ee09b3b64c76fb6fdba
Marquess Valdez <marquessavaldez@gmail.com>
2023-11-22 10:14:41 -0800

    move documentation workflow so it can be tested

--- quil-py/src/validation/mod.rs
+++ quil-py/src/validation/mod.rs
@@ -1,7 +1,15 @@
+use pyo3::{pyfunction, PyResult};
+
 use rigetti_pyo3::create_init_submodule;
 
 pub mod identifier;
 
 create_init_submodule! {
-    submodules : ["identifier": identifier::init_submodule],
+    funcs: [dummy],
+    submodules: ["identifier": identifier::init_submodule],
+}
+
+#[pyfunction]
+fn dummy() -> PyResult<()> {
+    Ok(())
 }
