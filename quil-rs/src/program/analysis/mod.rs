097e4674ca0d8735e79b581303802f4896de5fa6
kalzoo <22137047+kalzoo@users.noreply.github.com>
2024-02-09 21:56:39 -0800

    export QubitGraph

--- quil-rs/src/program/analysis/mod.rs
+++ quil-rs/src/program/analysis/mod.rs
@@ -3,6 +3,7 @@ mod control_flow_graph;
 mod qubit_graph;
 
 pub use control_flow_graph::{BasicBlock, BasicBlockTerminator, ControlFlowGraph};
+pub use qubit_graph::QubitGraph;
 
 #[cfg(test)]
 pub(crate) mod test_programs;
