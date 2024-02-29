35e80739c2a6ef5f9b9cace76a92bd30a0bdfe15
kalzoo <22137047+kalzoo@users.noreply.github.com>
2024-02-29 13:23:39 -0800

    remove unused Python binding

--- quil-py/src/program/scheduling.rs
+++ quil-py/src/program/scheduling.rs
@@ -1,19 +1,10 @@
 use pyo3::exceptions::PyValueError;
 use quil_rs::program::scheduling::{
-    ComputedScheduleError, ComputedScheduleItem, ScheduleSeconds, ScheduledBasicBlock,
-    ScheduledBasicBlockOwned, Seconds, TimeSpan,
+    ComputedScheduleError, ComputedScheduleItem, ScheduleSeconds, Seconds, TimeSpan,
 };
 use rigetti_pyo3::{
-    impl_repr, py_wrap_error, py_wrap_type, pyo3::prelude::*, wrap_error, PyWrapper, ToPythonError,
+    impl_repr, py_wrap_error, py_wrap_type, pyo3::prelude::*, wrap_error, PyWrapper,
 };
-
-use super::PyProgram;
-
-py_wrap_type! {
-    PyScheduledBasicBlock(ScheduledBasicBlockOwned) as "ScheduledBasicBlock"
-}
-
-impl_repr!(PyScheduledBasicBlock);
 
 wrap_error!(RustComputedScheduleError(ComputedScheduleError));
 py_wrap_error!(
@@ -22,17 +13,6 @@ py_wrap_error!(
     PyComputedScheduleError,
     PyValueError
 );
-
-impl PyScheduledBasicBlock {
-    pub fn as_schedule_seconds(&self, program: &PyProgram) -> PyResult<PyScheduleSeconds> {
-        ScheduledBasicBlock::from(&self.0)
-            .as_schedule_seconds(&program.0)
-            .map(Into::into)
-            .map_err(RustComputedScheduleError::from)
-            .map_err(RustComputedScheduleError::to_py_err)
-    }
-}
-
 py_wrap_type! {
     PyScheduleSeconds(ScheduleSeconds) as "ScheduleSeconds"
 }
