de94d05d944715205eb1a9838fea74353a01b12f
kalzoo <22137047+kalzoo@users.noreply.github.com>
2024-07-29 15:27:36 -0700

    fix doc example

--- quil-py/quil/program/__init__.pyi
+++ quil-py/quil/program/__init__.pyi
@@ -6,62 +6,63 @@ The `quil.program` module contains classes for constructing and representing a Q
 ## Source Mapping for Calibration Expansion
 
 ```py
-    import inspect
+import inspect
+from quil.program import Program
 
-    program_text = inspect.cleandoc(
-        \"\"\"
-        DEFCAL X 0:
-            Y 0
+program_text = inspect.cleandoc(
+    \"\"\"
+    DEFCAL X 0:
+        Y 0
 
-        DEFCAL Y 0:
-            Z 0
+    DEFCAL Y 0:
+        Z 0
 
-        X 0 # This instruction is index 0
-        Y 0 # This instruction is index 1
-        \"\"\"
-    )
+    X 0 # This instruction is index 0
+    Y 0 # This instruction is index 1
+    \"\"\"
+)
 
-    # First, we parse the program and expand its calibrations
-    program = Program.parse(program_text)
-    expansion = program.expand_calibrations_with_source_map()
-    source_map = expansion.source_map()
+# First, we parse the program and expand its calibrations
+program = Program.parse(program_text)
+expansion = program.expand_calibrations_with_source_map()
+source_map = expansion.source_map()
 
-    # This is what we expect the expanded program to be. X and Y have been replaced by Z
-    expected_program_text = inspect.cleandoc(
-        \"\"\"
-        DEFCAL X 0:
-            Y 0
+# This is what we expect the expanded program to be. X and Y have been replaced by Z
+expected_program_text = inspect.cleandoc(
+    \"\"\"
+    DEFCAL X 0:
+        Y 0
 
-        DEFCAL Y 0:
-            Z 0
+    DEFCAL Y 0:
+        Z 0
 
-        Z 0 # This instruction is index 0
-        Z 0 # This instruction is index 1
-        \"\"\"
-    )
+    Z 0 # This instruction is index 0
+    Z 0 # This instruction is index 1
+    \"\"\"
+)
 
-    # In order to discover _which_ calibration led to each Z in the resulting program, we
-    # can interrogate the expansion source mapping:
+# In order to discover _which_ calibration led to each Z in the resulting program, we
+# can interrogate the expansion source mapping:
 
-    # The X at index 0 should have been replaced with a Z at index 0:
+# The X at index 0 should have been replaced with a Z at index 0:
 
-    # ...first, we list the calibration expansion targets for that first instruction...
-    targets = source_map.list_targets_for_source_index(0)
+# ...first, we list the calibration expansion targets for that first instruction...
+targets = source_map.list_targets_for_source_index(0)
 
-    # ...then we extract the expanded instruction.
-    # If the instruction had _not_ been expanded (i.e. there was no matching calibration), then `as_expanded()` would return `None`.
-    expanded = targets[0].as_expanded()
+# ...then we extract the expanded instruction.
+# If the instruction had _not_ been expanded (i.e. there was no matching calibration), then `as_expanded()` would return `None`.
+expanded = targets[0].as_expanded()
 
-    # ...This line shows how that `X 0` was expanded into instruction index 0 (only) within the expanded program.
-    # The end of the range is exclusive.
-    assert expanded.range() == range(0, 1)
+# ...This line shows how that `X 0` was expanded into instruction index 0 (only) within the expanded program.
+# The end of the range is exclusive.
+assert expanded.range() == range(0, 1)
 
-    # We can also map instructions in reverse: given an instruction index in the expanded program, we can find the source index.
-    # This is useful for understanding the provenance of instructions in the expanded program.
-    sources = source_map.list_sources_for_target_index(1)
+# We can also map instructions in reverse: given an instruction index in the expanded program, we can find the source index.
+# This is useful for understanding the provenance of instructions in the expanded program.
+sources = source_map.list_sources_for_target_index(1)
 
-    # ... in this case, the instruction was expanded from the source program at index 1.
-    assert sources == [1]
+# ... in this case, the instruction was expanded from the source program at index 1.
+assert sources == [1]
 ```
 """
 
