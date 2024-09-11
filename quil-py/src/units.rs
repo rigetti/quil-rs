d688d6a6bce5be3033f23dbae26f1b309266c691
Marquess Valdez <marquessavaldez@gmail.com>
2024-09-11 11:25:48 -0700

    feat(py): Add `waveforms` module for generating discrete IQ value sequences representing Quil's defined set of waveforms.

---
+++ quil-py/src/units.rs
@@ -0,0 +1,14 @@
+use pyo3::prelude::*;
+
+use quil_rs::units::{Cycles, Radians};
+use rigetti_pyo3::py_wrap_type;
+
+py_wrap_type! {
+    #[derive(Debug)]
+    PyCycles(Cycles<f64>) as "Cycles"
+}
+
+py_wrap_type! {
+    #[derive(Debug)]
+    PyRadians(Radians<f64>) as "Radians"
+}
